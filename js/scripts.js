de3bff78d74649b27017fbdc16b0255523af9447
Cheglok <kopytovmikhail@yandex.ru>
2018-04-03 23:22:54 +0500

    И всё-таки скрипты не в одном файле

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,3 @@
-var bucketLink = document.querySelector(".button-buy");
-
-var bucketPopup = document.querySelector(".modal-bucket");
-
-var bucketClose = bucketPopup.querySelector(".modal-close");
-
-var bucketCarry = bucketPopup.querySelector(".button-modal-carry");
-
-bucketLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  bucketPopup.classList.add("modal-show");
-});
-
-bucketClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  bucketPopup.classList.remove("modal-show");
-});
-
-bucketCarry.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  bucketPopup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (bucketPopup.classList.contains("modal-show")) {
-      bucketPopup.classList.remove("modal-show");
-    }
-  }
-});
-
 var link = document.querySelector(".write-us-open");
 
 var popup = document.querySelector(".modal-write-us");
