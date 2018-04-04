54c57769cd4501ee0853da17d3db27387aa10a5c
Cheglok <kopytovmikhail@yandex.ru>
2018-04-04 07:48:42 +0500

    Минимизировал CSS JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+var bucketLink = document.querySelector(".button-buy");
+
+var bucketPopup = document.querySelector(".modal-bucket");
+
+var close = bucketPopup.querySelector(".modal-close");
+
+var bucketCarry = bucketPopup.querySelector(".button-modal-carry");
+
+bucketLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  bucketPopup.classList.add("modal-show");
+});
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  bucketPopup.classList.remove("modal-show");
+});
+bucketCarry.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  bucketPopup.classList.remove("modal-show");
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (bucketPopup.classList.contains("modal-show")) {
+      bucketPopup.classList.remove("modal-show");
+    }
+  }
+});
+
 var link = document.querySelector(".write-us-open");
 
 var popup = document.querySelector(".modal-write-us");
